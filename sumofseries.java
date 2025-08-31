3de004e47d412b64b7d40e22d126ad72c0b1d9b3
soumya2491 <soumyachopra901@gmail.com>
2025-08-31 21:14:01 +0530

    prac java

---
+++ sumofseries.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class sumofseries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i%2==0) {
+                sum-=i;
+            }else sum+=i;
+        }
+        System.out.println(sum);
+        input.close();
+    }
+}
